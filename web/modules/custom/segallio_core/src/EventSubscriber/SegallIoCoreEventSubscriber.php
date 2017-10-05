9e623cdb10cf1f6055af8af86a240b5033e280fc
Roy Segall <roy.segall@gizra.com>
2017-10-06 02:35:16 +0300

    Pulling info form social networks.

--- web/modules/custom/segallio_core/src/EventSubscriber/SegallIoCoreEventSubscriber.php
+++ web/modules/custom/segallio_core/src/EventSubscriber/SegallIoCoreEventSubscriber.php
@@ -2,8 +2,8 @@
 
 namespace Drupal\segallio_core\EventSubscriber;
 
+use Drupal\social_auth_facebook\FacebookAuthManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -25,6 +25,10 @@ class SegallIoCoreEventSubscriber implements EventSubscriberInterface {
    * Initializes bargain core module requirements.
    */
   public function onRequest(GetResponseEvent $event) {
+//    $foo = \Drupal::service('social_auth_facebook.persistent_data_handler');
+    /** @var FacebookAuthManager $foo */
+    $foo = \Drupal::service('social_auth_facebook.manager');
+
   }
 
 }
