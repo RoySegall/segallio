a0e2ba7a78926863fb10b59fd60b13dd2775736f
Roy Segall <roy.segall@gizra.com>
2017-10-06 11:22:26 +0300

    Managed with the first snippet!

--- web/modules/custom/segallio_core/src/EventSubscriber/SegallIoCoreEventSubscriber.php
+++ web/modules/custom/segallio_core/src/EventSubscriber/SegallIoCoreEventSubscriber.php
@@ -3,6 +3,9 @@
 namespace Drupal\segallio_core\EventSubscriber;
 
 use Drupal\social_auth_facebook\FacebookAuthManager;
+use Drupal\social_auth_facebook\FacebookAuthPersistentDataHandler;
+use Facebook\Facebook;
+use Facebook\FacebookResponse;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -25,9 +28,19 @@ class SegallIoCoreEventSubscriber implements EventSubscriberInterface {
    * Initializes bargain core module requirements.
    */
   public function onRequest(GetResponseEvent $event) {
-//    $foo = \Drupal::service('social_auth_facebook.persistent_data_handler');
-    /** @var FacebookAuthManager $foo */
-    $foo = \Drupal::service('social_auth_facebook.manager');
+    /** @var FacebookAuthPersistentDataHandler $facebook_data_persistant */
+    $facebook_data_persistant = \Drupal::service('social_auth_facebook.persistent_data_handler');
+
+    /** @var FacebookAuthManager $facebookManager */
+    $networkManager = \Drupal::service('plugin.network.manager');
+
+    /** @var Facebook $facebook */
+    $facebook = $networkManager->createInstance('social_auth_facebook')->getSdk();
+
+    /** @var FacebookResponse $response */
+    $response = $facebook->get('me?fields=posts{permalink_url,comments,reactions,full_picture,message,shares}', $facebook_data_persistant->get('access_token')->getValue());
+
+    dpm($response->getGraphNode()->asArray()['posts'][0]);
 
   }
 
