8ec6c969164aae6f721cac34c549d3d57444b03c
Roy Segall <roy.segall@gizra.com>
2017-11-11 21:11:42 +0200

    Add comments for some methods.

--- web/modules/custom/segallio_puller/src/Plugin/PullerTrait.php
+++ web/modules/custom/segallio_puller/src/Plugin/PullerTrait.php
@@ -7,10 +7,28 @@ namespace Drupal\segallio_puller\Plugin;
  */
 trait PullerTrait {
 
+  /**
+   * Download the image and create a file object.
+   *
+   * @param $value
+   *   The value of the field.
+   *
+   * @return mixed
+   *   The file object.
+   */
   public function copyImage($value) {
     return system_retrieve_file($value, NULL, TRUE, FILE_EXISTS_REPLACE);
   }
 
+  /**
+   * Get the total reaction for a given entry.
+   *
+   * @param $value
+   *   The value of the field.
+   *
+   * @return mixed
+   *   The number of reactions.
+   */
   public function countReactions($value) {
     if (empty($value['summary'])) {
       return 0;
@@ -19,6 +37,15 @@ trait PullerTrait {
     return $value['summary']['total_count'];
   }
 
+  /**
+   * Get the total comments for a given entry.
+   *
+   * @param $value
+   *   The value of the field.
+   *
+   * @return mixed
+   *   The number of comments.
+   */
   public function countComments($value) {
     if (empty($value['summary'])) {
       return 0;
@@ -27,6 +54,15 @@ trait PullerTrait {
     return $value['summary']['total_count'];
   }
 
+  /**
+   * Get the total shares sfor a given entry.
+   *
+   * @param $value
+   *   The value of the field.
+   *
+   * @return mixed
+   *   The number of shares.
+   */
   public function countShares($value) {
     if (empty($value['count'])) {
       return 0;
