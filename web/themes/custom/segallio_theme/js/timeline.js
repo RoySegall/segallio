eb80c76f1f9dd1cf9d7a60445f4cb4f00a9c9ab1
Roy Segall <roy.segall@gizra.com>
2018-01-26 23:13:31 +0200

    Commit more code.

--- web/themes/custom/segallio_theme/js/timeline.js
+++ web/themes/custom/segallio_theme/js/timeline.js
@@ -3,18 +3,36 @@ app = new Vue({
   el: '#timeline',
   data: {
     posts: [
-      {type: 'facebook', 'text': 'This is a facebook post'},
-      {type: 'github', 'text': 'This is a github post'},
-      {type: 'instagram', 'text': 'This is a instagram post'},
+      {type: 'facebook', 'title': 'Post', 'text': 'This is a facebook post'},
+      {type: 'github', 'title': 'Post', 'text': 'This is a github post'},
+      {type: 'instagram','title': 'Post',  'text': 'This is a instagram post'},
       {type: 'twitter', 'text': 'This is a twitter post'},
       {type: 'facebook', 'text': 'This is a facebook post'},
     ],
-  }
-});
+  },
 
-Vue.directive('social-icon', {
-  bind: function (el, binding, vnode) {
-    var s = JSON.stringify;
-    el.innerHTML = "a";
+  directives: {
+    'social-icon': {
+      // directive definition.
+      inserted: function (el, binding, vnode) {
+        switch (binding.value) {
+          case 'facebook':
+            el.innerHTML = "<i class='fab fa-facebook-f'></i>";
+            break;
+
+          case 'github':
+            el.innerHTML = "<i class='fab fa-github-alt'></i>";
+            break;
+
+          case 'instagram':
+            el.innerHTML = "<i class='fab fa-instagram'></i>";
+            break;
+
+          case 'twitter':
+            el.innerHTML = "<i class='fab fa-twitter'></i>";
+            break;
+        }
+      }
+    }
   }
 });
