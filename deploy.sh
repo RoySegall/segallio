1a8ddbb32b959890b3692d26c7efd223ec30f461
Roy Segall <roy@segall.io>
2019-02-24 16:12:28 +0200

    Updating site

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo -t tahini
+echo -e "\033[0;32mPassing CNAME to the doc folder\033[0m"
+cp CNAME docs
+git add .
+
+git commit -m "Updating site"
+git push
+
+echo -e "\033[0;32mDone!\033[0m"
