bbe124f6a1133a060204e8d9b8a521d25d28c829
Roy Segall <roy@segall.io>
2019-02-26 17:00:53 +0200

    Adding stuff

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo -t tahini
 echo -e "\033[0;32mPassing CNAME to the doc folder\033[0m"
-cp CNAME complies
-cd complies
+cp CNAME compiles
+cd compiles
 git add .
 
 git commit -m "Updating site"
