377620e9e44d5eb22b6f3768a3d329e1fbd79065
RoySegall <roy@segall.io>
2020-07-20 22:43:27 +0300

    Working on the recent blog posts

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="h-screen w-screen pl-32 pr-32 pt-10 text-left blogs">
         <h2 class="text-3xl"><b>Recent blog posts</b></h2>
-        <hr class="mb-10"/>
+        <p class="text-xl pt-2 pb-2">Here you read various blog posts I wrote over the years: From my time at Gizra, and my blog posts at Medium.com</p>
 
-        <div v-for="blog in blogs" class="text-left mb-4">
-            <div class="pb-4"><a :href=blog.url target="_blank"><b>{{blog.title}}</b></a></div>
+        <hr class="mb-6"/>
+
+
+        <div v-for="blog in blogs" class="text-left mb-4 blog">
+            <div class="pb-4"><a :href=blog.url target="_blank" class="text-blue-400 text-2xl hover:underline"><b>{{blog.title}}</b></a>
+            </div>
 
             <div class="pb-4">
-                <span class="date">Published on:</span> {{blog.date}}, <span class="source">Published in:</span> {{blog.source}}
+                <span class="text-blue-800 font-bold">Published on:</span> {{blog.date}}, <span
+                    class="text-orange-500 font-bold">Published in:</span> {{blog.source}}
             </div>
 
             <p class="pb-4">
                 {{blog.intro}}
             </p>
 
-            <hr />
+            <hr/>
         </div>
     </div>
 </template>
@@ -32,6 +37,27 @@
 
 <style lang="scss">
     .blogs {
+
+        .blog {
+
+            hr {
+                width: 60vh;
+                margin: 0 auto;
+            }
+
+            &:nth-child(even) {
+                hr {
+                    border-bottom: solid 1px #f7b75e;
+                }
+            }
+
+            &:nth-child(odd) {
+                hr {
+                    border-bottom: solid 1px #6bcff1;
+                }
+            }
+        }
+
         hr {
             border-bottom: solid 1px #f7b75e;
         }
