c2ad247f74ee51a31c89cb90b5d8f29727d50cf7
RoySegall <roy@segall.io>
2020-08-01 00:46:25 +0300

    Adding texts to the contribution section

--- src/components/Contributions/Technologies.vue
+++ src/components/Contributions/Technologies.vue
@@ -1,11 +1,16 @@
 <template>
   <ul class="flex filter text-white text-5xl">
-    <li class="pr-6">Filter by:</li>
-    <li v-for="(technology, index) in technologies" :key=index  class="pr-6">
+    <li class="pr-6">
+      Filter by:
+      <span class="block text-xs">Click on the icon to filter contributions.</span>
+      <span class="block text-xs">Click again to disable the filter.</span>
+    </li>
+    <li v-for="(technology, index) in technologies" :key=index  class="pr-16">
       <a class="cursor-pointer"
          v-bind:class="{'text-black': technology.name == selectedTech}"
          v-on:click="$emit('filter-by-lang', technology.name)">
         <font-awesome-icon class="side-menu-icon" :icon="[technology.category, technology.icon]"/>
+        <span class="block text-xs">{{technology.name}}</span>
       </a>
     </li>
   </ul>
