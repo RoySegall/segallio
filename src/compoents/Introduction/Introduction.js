1c412ec7468579832dc1ea56b198c07b673a340f
RoySegall <roy@segall.io>
2020-08-15 09:11:02 +0300

    Fixing something mingor

--- src/compoents/Introduction/Introduction.js
+++ src/compoents/Introduction/Introduction.js
@@ -4,29 +4,28 @@ import { StaticQuery, graphql } from "gatsby"
 import {Social} from "./Social";
 
 const query = graphql`
-  {
-    file(name: {eq: "introduction"}) {
-      childImageSharp {
-        fluid {
-          src
+      {
+        file(name: {eq: "introduction"}) {
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
         }
       }
-    }
-  }
-`
+    `;
 
-const Introduction = ({profileSrc}) => <div className="introduction first-section">
+const Introduction = ({imageSrc}) => <div className="introduction first-section">
     <h1 className="introduction-h1">Hello, I'm Roy Segall</h1>
     <p className="introduction-p">
         Enthusiastic, self-learned, highly
         motivated, love to explore new technologies
     </p>
     <p className="introduction-p">and contributing to open-source projects.</p>
-    <img alt="Funny picture of mu self" src={profileSrc} />
-
+    <img alt="Funny picture of mu self" src={imageSrc} />
     <Social />
 </div>
 
 export default () => (
-    <StaticQuery query={query} render={data => <Introduction profileSrc={data.file.childImageSharp.fluid.src}/>} />
+    <StaticQuery query={query} render={data => <Introduction imageSrc={data.file.childImageSharp.fluid.src}/> } />
 )
