a0d23154b5f1bc7437b3149f6a670b5a28e5afb4
RoySegall <roy@testim.io>
2023-05-29 19:36:22 +0300

    Remoe the menui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import styles from './page.module.scss';
-import {Menu} from "@/Components/Menu";
 import {Introduction} from "@/Components/Introduction";
 import {Jobs} from "@/Components/Jobs";
 import {BlogPosts} from "@/Components/BlogPosts";
 
 export default function Home() {
     return <div className={styles.home}>
-        {/*<Menu />*/}
         <Introduction />
         <Jobs />
         <BlogPosts />
