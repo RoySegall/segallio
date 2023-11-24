85dd68083cd0d87f340e04bfa766a398fcd8bda0
RoySegall <roy@testim.io>
2023-11-24 10:00:05 +0200

    Make the work cooler

--- src/Components/Introduction/Introduction.tsx
+++ src/Components/Introduction/Introduction.tsx
@@ -13,23 +13,43 @@ const myMessages = [
     "I'm married and own two cats: Sam and freddy",
     "I'm working in Tricentis israel as a full stack developer",
     "I used to contribute to open source projects and gave session at meetups but I'm not doing it anymore as I'm focusing on my family and my work",
-    "You can ping me up at Linkedin, facebook, twitter or github",
 ];
 
+
+// actions:
+// 1. are you free for hire?
+//    no :)
+//    Are you sure?
+//    yes
+//    Really really sure?
+//    yes... but you can try again in a the future. Maybe soemthing will change.
+// 2. what is you stack?
+//    I'm a full stack developer, I can do everything. In my last(current) job I did some ux ui issues I think i'm not just full stack developer.
+//    but what is the tech?
+//    I used to work with Drupal for a lot of time (you can read in the job sections, link). I worked with python in Dreamed (link to job and mark dreamed) and now I'm doing react, node, a bit of appium.
+// 3. I want to see you story in the industry (link to jobs)
+// 4. I want to read your blog posts (link to blog posts)
+// 5. Where can I catch you?
+
+const Actions = () => {
+    return <>asdasdasd</>
+};
+
 export const Introduction = () => {
-    const [messages, setMessages] = useState<string[]>([]);
-    const [collapseIntroduction, setCollapseIntroduction] = useState<boolean>(false);
+    const [messages, setMessages] = useState<string[]>([myMessages[0]]);
+    const [collapseIntroduction, setCollapseIntroduction] = useState(false);
+    const [showActions, setShowActions] = useState(true);
 
-    useEffect(() => {
-        (async () => {
-            for (let i = 0; i < myMessages.length; i++) {
-                setMessages(messages => [...messages, myMessages[i]]);
-                await sleep(2);
-            }
-
-            setCollapseIntroduction(true);
-        })();
-    }, []);
+    // useEffect(() => {
+    //     (async () => {
+    //         for (let i = 0; i < myMessages.length; i++) {
+    //             setMessages(messages => [...messages, myMessages[i]]);
+    //             await sleep(2);
+    //         }
+    //
+    //         setCollapseIntroduction(true);
+    //     })();
+    // }, []);
 
     return <div className={`${styles.introductionSection} ${collapseIntroduction && styles.collapseSection}`}>
         <div className={`${styles.introductionWrapper} ${collapseIntroduction && styles.collapseSection}`}>
@@ -44,6 +64,8 @@ export const Introduction = () => {
                 <div className={styles.messages}>
                     {messages.map((message, key) => <Message message={message} key={key} />)}
                 </div>
+
+                {showActions && <Actions />}
 
                 <div  className={`${styles.inputWrapper} ${collapseIntroduction && styles.collapseSection}`}>
                     <input placeholder='Say something nice :)' />
