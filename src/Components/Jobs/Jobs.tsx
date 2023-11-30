b6c6b4d3c84933238692602d9ddece3623c45e9a
RoySegall <roy@testim.io>
2023-11-30 07:30:57 +0200

    More work on jobs

--- src/Components/Jobs/Jobs.tsx
+++ src/Components/Jobs/Jobs.tsx
@@ -1,10 +1,28 @@
 'use client';
 
-import {useMemo, useState} from "react";
+import ReactChild, {FC, useEffect, useMemo, useRef, useState} from "react";
 import styles from './jobs.module.scss';
 import {robotoMono} from "@/common/fonts";
 import Image from 'next/image';
-import {jobs as jobEntries} from './data'
+import {jobs as jobEntries} from './data';
+
+const Job: FC<{ image: string, name: string, paragraphs: ReactChild.JSX.Element[]}> = ({name, image, paragraphs}) => {
+    const paragraphsRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (paragraphsRef.current) {
+            paragraphsRef.current.scroll(0, 0)
+        }
+    }, [name]);
+
+    return <div className={styles.job} >
+        <Image src={image} height={70} alt={name}/>
+
+        <div className={styles.paragraphs} ref={paragraphsRef}>
+            {paragraphs.map((paragraph, index) => <p key={index}>{paragraph}</p>)}
+        </div>
+    </div>;
+}
 
 export const Jobs = () => {
     const jobs = [
@@ -24,7 +42,8 @@ export const Jobs = () => {
                 <div className={styles.box}>
                     <div className={styles.container}>
                         <div className={styles.cards}>
-                            {jobs.map((job, index) => <div className={styles.card} key={index} onClick={() => setJobId(job.id)}>
+                            {jobs.map((job, index) => <div className={styles.card} key={index}
+                                                           onClick={() => setJobId(job.id)}>
                                     <div className={`${styles.dot} ${job.id === jobId && styles.active}`}></div>
                                     <h4>{job.name}</h4>
                                     <p>{job.position}, {job.period.start} {job.period.end && `- ${job.period.end}`}</p>
@@ -34,13 +53,7 @@ export const Jobs = () => {
                     </div>
                 </div>
             </div>
-            <div className={styles.job}>
-                <Image src={selectedJob.image} height={70} alt={selectedJob.name} />
-
-                <div className={styles.paragraphs}>
-                    {selectedJob.paragraphs.map((paragraph, index) => <p key={index}>{paragraph}</p>)}
-                </div>
-            </div>
+            <Job name={selectedJob.name} image={selectedJob.image} paragraphs={selectedJob.paragraphs} />
         </div>
 
     </div>
